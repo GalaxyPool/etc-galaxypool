404200d79fb0d82244bd6d247a581e2a22972e95
minerpoolcf <41727706+minerpoolcf@users.noreply.github.com>
2018-08-21 01:41:49 +0700

    update earnperday

---
+++ www/app/controllers/payments.js
@@ -0,0 +1,5 @@
+import Ember from 'ember';
+export default Ember.Controller.extend({
+  applicationController: Ember.inject.controller('application'),
+  config: Ember.computed.reads('applicationController.config')
+});
